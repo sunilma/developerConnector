67a761011f8dec2120aceda0fe2eb8545886ae71
Sunil Maharjan <ratnasunil70@gmail.com>
2020-02-20 21:35:14 +0200

    backend with nodejs completed

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 //initialize app
 const app = express();
 
+// Connecting the database
+connectDB();
+
+// Initialize middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API running'));
+
+//Defining routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 //initialize port 
 const PORT = process.env.PORT || 5000;
