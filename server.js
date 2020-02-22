4f8dc033ff8ebe7b5a67ddf2753ef2c796692bba
Sunil Maharjan <ratnasunil70@gmail.com>
2020-02-22 12:26:00 +0200

    app ready for production

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+// for production usage
+const path =require('path');
+
 //initialize app
 const app = express();
 
@@ -10,7 +13,6 @@ connectDB();
 // Initialize middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API running'));
 
 //Defining routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,6 +20,16 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production' ) {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 //initialize port 
 const PORT = process.env.PORT || 5000;
